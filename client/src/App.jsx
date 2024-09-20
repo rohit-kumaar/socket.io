6475b8144d65165051e23c3a4065201f0b2114f3
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2024-09-20 12:24:11 +0530

    socket is connected with client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { io } from "socket.io-client";
 
 export default function App() {
+  const socket = io(`http://localhost:4000`);
+
   return <div>Vite app is working</div>;
 }
--- server/server.js
+++ server/server.js
@@ -1,12 +1,36 @@
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 
-const server = express();
 const port = 4000;
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
-server.get("/", (req, res) => {
+app.use(
+  cors({  
+    origin: `http://localhost:5173`,
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
   res.status(200).send("Hello World");
 });
 
+io.on("connection", (socket) => {
+  console.log("User Connected");
+  console.log("Id", socket.id);
+});
+
 server.listen(port, () => {
-  console.log(`Port is working in port : http://localhost:${port}/`);
+  console.log(`Port is working in port : http://localhost:${port}`);
 });
